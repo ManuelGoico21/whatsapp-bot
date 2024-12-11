c732871e3a8716023c6e2fc7e66fad9a244e6bb1
ManuelGoico21 <maotto12@gmail.com>
2024-12-11 18:15:34 -0400

    WhatsApp Bot

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Lista de IDs de grupos permitidos
+const allowedGroupIds = [
+    '120363312830003816@g.us'
+];
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    console.log('Escanea este QR con tu teléfono:');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('El cliente está listo!');
+});
+
+client.on('message', async (msg) => {
+    const chat = await msg.getChat();
+
+    // Verificamos si el mensaje viene de un grupo y si el ID del grupo está en la lista permitida
+    if (chat.isGroup && allowedGroupIds.includes(chat.id._serialized)) {
+        // Aquí tu lógica para los grupos permitidos
+        if (msg.body === 'ping') {
+            msg.reply('pong');
+        }
+    }
+});
+
+client.initialize();
